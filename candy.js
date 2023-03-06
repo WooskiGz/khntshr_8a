b0789a7f93abc2c24a7ba02e360062f2f750a680
WooskiGz <126443457+WooskiGz@users.noreply.github.com>
2023-03-06 13:53:15 +0800

    Add files via upload

---
+++ candy.js
@@ -0,0 +1,7 @@
+let une = 1000;
+function yomom() {
+  let chiher = document.getElementById("chiher").value;
+  let too = document.getElementById("too").value;
+  let tulbur = une * too;
+  document.getElementById("totalPrice").innerHTML = tulbur;
+}
